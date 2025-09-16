0b5e1e25330ef728535178b29ba91e725e46635a
Timothy Vriesema <timothy.vriesema@technology1.com>
2025-09-16 22:23:57 +1000

    Update compiler

--- compile_qmk.sh
+++ compile_qmk.sh
@@ -1,2 +1,2 @@
 qmk compile -e CONVERT_TO=liatris -kb splitkb/kyria/rev3 -km tripix
-qmk compile -e CONVERT_TO=liatris -kb crkbd/rev1 -km tripix
+qmk compile -e CONVERT_TO=kb2040 -kb crkbd/rev1 -km tripix
