0d1c85f504d90e28368946db2408527887a626c5
Timothy Vriesema <tripix@msn.com>
2026-04-20 07:40:41 +1000

    Updated sofle

--- compile_qmk.sh
+++ compile_qmk.sh
@@ -1,4 +1,4 @@
 qmk compile -e CONVERT_TO=liatris -kb splitkb/kyria/rev3 -km tripix
 qmk compile -e CONVERT_TO=liatris -kb splitkb/kyria/rev3 -km default
 qmk compile -e CONVERT_TO=kb2040 -kb crkbd/rev1 -km tripix
-qmk compile -e CONVERT_TO=kb2040 -kb crkbd/rev1 -km tripix
+qmk compile -e CONVERT_TO=rp2040_ce -kb sofle -km tripix 
--- keyboards/sofle/keymaps/tripix/rules.mk
+++ keyboards/sofle/keymaps/tripix/rules.mk
@@ -1,1 +1,3 @@
 TRI_LAYER_ENABLE = yes
+RGBLIGHT_ENABLE = yes   # For underglow and general lighting
+RGB_MATRIX_ENABLE = yes # Required for per-key RGB lighting
